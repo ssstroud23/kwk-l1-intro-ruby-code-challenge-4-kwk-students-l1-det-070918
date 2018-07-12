7c6b1ec8a10bfe7850a97234ff9acb8abcf569c6
ssstroud23 <ssstroud23@gmail.com>
2018-07-12 16:08:38 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -6,18 +6,22 @@ coins = {:quarter => 0, :dimes => 0, :nickels => 0, :pennies => 0}
 while cents >= 25 
 coins[:quarter] += 1 
 cents -= 25 
+end 
 
 while cents >= 10
 coins[:dime] += 1 
 cents -= 10 
+end 
 
 while cents >= 5 
 coins[:nickel] += 1
 cents -= 5 
+end 
 
 while cents >= 1 
 coins[:penny] += 1
 cents -= 1 
+end 
 
 
 end
